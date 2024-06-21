1cb37eb15e9b8b793dd021c29471d43e53d2a571
Shen <xjxjcldk9@gmail.com>
2024-06-22 00:19:56 +0800

    完善台股的code

--- code/direct_portfolio_evaluation/main.py
+++ code/direct_portfolio_evaluation/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pandas as pd
 import yfinance as yf
 from datetime import datetime
@@ -6,11 +7,17 @@ from argparse import ArgumentParser
 import warnings
 warnings.filterwarnings("ignore")
 
+TW_ETF = Path(__file__).parents[2] / 'data' / 'ETFs' / 'combined_TW.csv'
+
+
+data_storage = Path(__file__).parents[2] / 'data' / 'stock_prices'
+data_storage.mkdir(exist_ok=True)
+
+SAVE_FILE_TW = data_storage / 'TW_price.csv'
+
 
 parser = ArgumentParser()
-
 parser.add_argument('case', choices=['TW', 'US'])
-
 args = parser.parse_args()
 
 
@@ -21,26 +28,27 @@ prev_year = prev_year.strftime("%Y-%m-%d")
 
 
 def get_price(stock_name, market, start_date, end_date):
-
     if market == 'TW':
         tick = str(stock_name['代碼']) + '.TW'
     stock = yf.Ticker(tick)
 
     stock_data = stock.history(start=start_date, end=end_date)
-    return stock_data[['Close']]
+
+    prices = stock_data['Close']
+    return prices
 
 
 def get_all_price(market):
     if market == 'TW':
-        df = pd.read_csv(
-            '/Users/liaoshenxun/APPdevelopment/Stock Recommendation Project/data/ETFs/combined_TW.csv')
+        df = pd.read_csv(TW_ETF)
     if market == 'US':
         df = '../data/ETFs/combined_TW.csv'
 
-    return df.apply(get_price, 1, args=(market, prev_year, today))
+    price_df = df.apply(get_price, 1, args=(market, prev_year, today))
+    price_df = price_df.set_index(df['股票名稱'])
+
+    return price_df.T
 
 
 if __name__ == '__main__':
-
-    get_all_price(args.case).to_csv(
-        '/Users/liaoshenxun/APPdevelopment/Stock Recommendation Project/code/direct_portfolio_evaluation/test.csv', index=False)
+    get_all_price(args.case).to_csv(SAVE_FILE_TW)
