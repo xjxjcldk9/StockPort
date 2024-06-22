dfa00260c298c216073ea2c7a9d12f56e12fcc0d
Shen <xjxjcldk9@gmail.com>
2024-06-23 00:43:19 +0800

    feat: add pyport optimal portfolio for custom budget

--- code/direct_portfolio_evaluation/main.py
+++ code/direct_portfolio_evaluation/main.py
@@ -1,30 +1,52 @@
+import warnings
 from pathlib import Path
 import pandas as pd
 import yfinance as yf
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from argparse import ArgumentParser
-import warnings
+
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+from pypfopt import objective_functions
+
+
 warnings.filterwarnings("ignore")
 
 TW_ETF = Path(__file__).parents[2] / 'data' / 'ETFs' / 'combined_TW.csv'
 
 
-data_storage = Path(__file__).parents[2] / 'data' / 'stock_prices'
-data_storage.mkdir(exist_ok=True)
+PRICE_DATA = Path(__file__).parents[2] / 'data' / 'stock_prices'
+PRICE_DATA.mkdir(exist_ok=True)
 
-SAVE_FILE_TW = data_storage / 'TW_price.csv'
-
+PORT_DATA = Path(__file__).parents[2] / 'data' / 'portfolio'
+PORT_DATA.mkdir(exist_ok=True)
 
 parser = ArgumentParser()
 parser.add_argument('case', choices=['TW', 'US'])
+parser.add_argument('-s', '--scrape', action='store_true')
+parser.add_argument('-v', '--value', required=True, type=int)
 args = parser.parse_args()
 
 
 today = datetime.today()
+ym = today.strftime("%Y-%m")
 prev_year = today-relativedelta(years=1)
 today = today.strftime("%Y-%m-%d")
 prev_year = prev_year.strftime("%Y-%m-%d")
+
+
+if args.case == 'TW':
+    FILE_NAME = f'TW_price_{ym}.csv'
+    PRICE_FILE = PRICE_DATA / FILE_NAME
+    PORT_FILE = PORT_DATA / f'TW_{ym}.csv'
+
+elif args.case == 'US':
+    FILE_NAME = f'US_price_{ym}.csv'
+    PRICE_FILE = PRICE_DATA / FILE_NAME
+    PORT_FILE = PORT_DATA / f'US_{ym}.csv'
 
 
 def get_price(stock_name, market, start_date, end_date):
@@ -50,9 +72,28 @@ def get_all_price(market):
     return price_df.T
 
 
-def optimal_portfolio(market):
-    pass
+def optimal_portfolio(price_df, value):
+    mu = mean_historical_return(price_df)
+    S = CovarianceShrinkage(price_df).ledoit_wolf()
+
+    ef = EfficientFrontier(mu, S)
+    ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+    w = ef.max_sharpe()
+
+    latest_prices = get_latest_prices(df)
+    da = DiscreteAllocation(w, latest_prices, total_portfolio_value=value)
+    allocation, leftover = da.lp_portfolio()
+    return pd.Series(allocation).to_frame().reset_index().rename(columns={'index': '股票名稱', 0: '推薦購買股數'})
 
 
 if __name__ == '__main__':
-    get_all_price(args.case).to_csv(SAVE_FILE_TW)
+    if args.scrape:
+        get_all_price(args.case).to_csv(PRICE_FILE)
+    try:
+        df = pd.read_csv(PRICE_FILE, index_col='Date')
+    except FileNotFoundError:
+        print(
+            f'Price file {FILE_NAME} does not found. Please scrape it first.')
+
+    portfolio_df = optimal_portfolio(df, args.value)
+    portfolio_df.to_csv(PORT_FILE)
